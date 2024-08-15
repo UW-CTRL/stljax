fd5901fc0ce279b35a8e948e3e56aa9b155b3815
Karen Leung <karen.ym.leung@gmail.com>
2024-08-15 15:04:12 -0700

    updated timing code

--- test_timing.py
+++ test_timing.py
@@ -12,20 +12,34 @@ if __name__ == "__main__":
     axis = 0
     pred_rev = Predicate('x', lambda x: x, axis, True)
     interval = [2, 5]
-    phi = Always(pred_rev > 4, interval=interval)
-    psi = AlwaysRecurrent(pred_rev > 4, interval=interval)
+    mask = Always(pred_rev > 4, interval=interval)
+    recurrent = AlwaysRecurrent(pred_rev > 4, interval=interval)
 
 
 
-    def grad_phi(signal):
-        return jax.vmap(jax.grad(lambda x: phi(x).mean()))(signal)
-    def grad_psi(signal):
-        return jax.vmap(jax.grad(lambda x: psi(x).mean()))(signal)
+    def grad_mask(signal):
+        return jax.vmap(jax.grad(lambda x: mask(x).mean()))(signal)
+    def grad_recurrent(signal):
+        return jax.vmap(jax.grad(lambda x: recurrent(x).mean()))(signal)
 
-    def phi_(signal):
-        return jax.vmap(lambda x: phi(x).mean())(signal)
-    def psi_(signal):
-        return jax.vmap(lambda x: psi(x).mean())(signal)
+    def mask_(signal):
+        return jax.vmap(lambda x: mask(x).mean())(signal)
+    def recurrent_(signal):
+        return jax.vmap(lambda x: recurrent(x).mean())(signal)
+
+
+    @jax.jit
+    def grad_mask_jit(signal):
+        return jax.vmap(jax.grad(lambda x: mask(x).mean()))(signal)
+    @jax.jit
+    def grad_recurrent_jit(signal):
+        return jax.vmap(jax.grad(lambda x: recurrent(x).mean()))(signal)
+    @jax.jit
+    def mask_jit(signal):
+        return jax.vmap(lambda x: mask(x).mean())(signal)
+    @jax.jit
+    def recurrent_jit(signal):
+        return jax.vmap(lambda x: recurrent(x).mean())(signal)
 
     # Number of loops per run
     loops = 100
@@ -38,30 +52,20 @@ if __name__ == "__main__":
     stds = []
     data = {}
 
+    # functions = ["mask_", "recurrent_", "grad_mask", "grad_recurrent", "mask_jit", "recurrent_jit", "grad_mask_jit", "grad_recurrent_jit"]
+    functions = ["mask_jit", "recurrent_jit", "grad_mask_jit", "grad_recurrent_jit"]
+
     while T <= 1024:
         print("running ", T)
         signal = jnp.array(np.random.random([bs, T]))
-        # %timeit jax.vmap(phi2, [0])(signal)
-        # foo(signal)
-
-        times1 = timeit.repeat("phi_(signal)", globals=globals(), repeat=runs, number=loops)
-        print("phi", statistics.mean(times1), statistics.stdev(times1))
-
-        times2 = timeit.repeat("psi_(signal)", globals=globals(), repeat=runs, number=loops)
-        print("psi", statistics.mean(times2), statistics.stdev(times2))
-
-        times3 = timeit.repeat("grad_phi(signal)", globals=globals(), repeat=runs, number=loops)
-        print("grad_phi", statistics.mean(times3), statistics.stdev(times3))
-
-        times4 = timeit.repeat("grad_psi(signal)", globals=globals(), repeat=runs, number=loops)
-        print("grad_psi", statistics.mean(times4), statistics.stdev(times4))
-
-        data[str(T)] = {"mask": times1, "recurrent": times2, "grad_mask": times3, "grad_recurrent": times4}
-        means.append([statistics.mean(times1), statistics.mean(times2), statistics.mean(times3), statistics.mean(times4)])
-        stds.append([statistics.stdev(times1), statistics.stdev(times2), statistics.stdev(times3), statistics.stdev(times4)])
-        print(T, means[-1], stds[-1])
+        times = []
+        data[str(T)] = {}
+        for f in functions:
+            print("timing ", f)
+            times.append(timeit.repeat(f + "(signal)", globals=globals(), repeat=runs, number=loops))
+            print("timing: ", statistics.mean(times[-1]), statistics.stdev(times[-1]))
+            data[str(T)][f] = times
+            with open('signal_length.pkl', 'wb') as f:
+                pickle.dump(data, f)
 
         T *= 2
-
-        with open('signal_length.pkl', 'wb') as f:
-            pickle.dump(data, f)
